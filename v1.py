e44c6c2c4ce6dbac2d29f65bc50c1c26ebe94bcb
appigoo <appigoodoc@gmail.com>
2025-07-26 22:49:14 +0800

    Update v1.py

--- v1.py
+++ v1.py
@@ -71,7 +71,7 @@ def backtest_strategy(data, initial_cash=100000):
             today['High'] > yesterday['High'] and
             today['Low'] > yesterday['Low'] and
             #today['Close'] > yesterday['Close'] and
-            today['Volume'] >= today['Volume_MA5']
+            #today['Volume'] >= today['Volume_MA5']
             #today['MACD'] > 0
         )
 
